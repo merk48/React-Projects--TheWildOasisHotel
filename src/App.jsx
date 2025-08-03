4a7756d5158af860538c7cdac8b355f7b8d71e88
Merk48 <meryKassis48@gmail.com>
2025-08-03 14:47:13 +0300

    feat: set-up react router and specify the app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
+import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Login from "./pages/Login";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
+import PageNotFound from "./pages/PageNotFound";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="login" element={<Login />} />
+          <Route path="account" element={<Account />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="users" element={<Users />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
+
+/*
 
 const StyledApp = styled.main`
   padding: 20px;
 `;
 
-function App() {
-  return (
-    <>
+ <>
       <GlobalStyles />
       <StyledApp>
         <Row type="vertical">
@@ -38,7 +65,4 @@ function App() {
         </Row>
       </StyledApp>
     </>
-  );
-}
-
-export default App;
+*/
