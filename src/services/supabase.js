b3bdd9d76a55f3ccebe18819b9137b9b30392aa2
Merk48 <meryKassis48@gmail.com>
2025-08-04 00:38:14 +0300

    feat: connect to supabase database

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://rvxjbtuvxrkqihpgutnf.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2eGpidHV2eHJrcWlocGd1dG5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyNDcyODIsImV4cCI6MjA2OTgyMzI4Mn0.8dFe6bIxPKzDrGy7SFnRJcgJkf1abQ1Mh4y5lA1VsUE";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
