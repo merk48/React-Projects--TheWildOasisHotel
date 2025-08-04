2f1f40a50c9f05f008f1d87efceef91dd70bb7a2
Merk48 <meryKassis48@gmail.com>
2025-08-04 16:22:10 +0300

    feat: set react-hook-form package

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,6 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-
       <BrowserRouter>
         <Routes>
           {/* Website pages
