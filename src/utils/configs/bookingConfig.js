0e143d303f9c2a84e4ddba5e0c798126f0f31dc1
Merk48 <meryKassis48@gmail.com>
2025-08-14 01:39:39 +0300

    refactor: booking configs

--- src/utils/configs/bookingConfig.js
+++ src/utils/configs/bookingConfig.js
@@ -1,6 +1,8 @@
-import { SUPABASE_METHODS } from "../supabaseQueryHelpers";
-import { PARAMS } from "./params";
+import { SUPABASE_METHODS } from "../helpers/supabaseQueryHelpers";
+import PARAMS from "../constants/urlParams";
+import { DATE_FORMAT } from "../constants/uiConstants";
 
+// Filters
 const STATUS = {
   ALL: "all",
   CHECKED_OUT: "checked-out",
@@ -8,56 +10,52 @@ const STATUS = {
   UNCONFIRMED: "unconfirmed",
 };
 
-// sort constant default
-const DEFAULT_SORT = "startDate-desc";
-
 // options for UI
-const statusOptions = [
+const STATUS_OPTIONS = [
   { value: STATUS.ALL, label: "All" },
   { value: STATUS.CHECKED_OUT, label: "Checked out" },
   { value: STATUS.CHECKED_IN, label: "Checked in" },
   { value: STATUS.UNCONFIRMED, label: "Unconfirmed" },
 ];
 
-const sortOptions = [
+const SORT_OPTIONS = [
   { value: "startDate-desc", label: "Date — recent first" },
   { value: "startDate-asc", label: "Date — earliest first" },
   { value: "totalPrice-desc", label: "Amount — high first" },
   { value: "totalPrice-asc", label: "Amount — low first" },
 ];
+// sort constant default
+const DEFAULT_SORT = SORT_OPTIONS[0].value;
 
 export const BOOKING_CONFIG = {
-  statusOptions: STATUS,
-  filters: {
-    status: {
-      field: "status", // backend field name
-      param: PARAMS.BOOKING.STATUS, // url param name
-      default: STATUS.ALL,
-      options: statusOptions,
-      toFilter: (value) =>
+  SORT_OPTIONS,
+  FILTERS: {
+    STATUS: {
+      FIELD: "status", // backend field name
+      PARAM: PARAMS.BOOKING.STATUS_FILTER, // url param name
+      DEFAULT: STATUS.ALL,
+      OPTIONS: STATUS_OPTIONS,
+      ToFilter: (value) =>
         !value || value === STATUS.ALL
           ? null
           : {
-              field: PARAMS.BOOKING.STATUS,
+              field: PARAMS.BOOKING.STATUS_FILTER,
               method: SUPABASE_METHODS.EQ,
               value,
             },
     },
   },
-  sort: {
-    options: sortOptions,
-    param: PARAMS.SORT, // url param name
-    default: DEFAULT_SORT,
+  SORT: {
+    OPTIONS: SORT_OPTIONS,
+    PARAM: PARAMS.SORT, // url param name
+    DEFAULT: DEFAULT_SORT,
   },
-  ui: {
-    statusTagColors: {
+  UI: {
+    STATUS_TAG_COLORS: {
       [STATUS.UNCONFIRMED]: "blue",
       [STATUS.CHECKED_IN]: "green",
       [STATUS.CHECKED_OUT]: "silver",
     },
-
-    dateFormats: {
-      display: "MMM dd yyyy",
-    },
+    DATE_FORMAT,
   },
 };
